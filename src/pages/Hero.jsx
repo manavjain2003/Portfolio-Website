a1df1c8fff070441420d8a6eef72edd1d6c9b39c
Manav Jain <manav7421@gmail.com>
2025-02-04 20:54:13 +0530

    4-02-2025

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <img/>
         <article class="content">
           <h1 class="heading">Welcome to My Portfolio</h1>
-          <h2 class="sub-heading" style={{fontSize:70}}>Hi, My name is Manav Jain</h2>
+          <h2 class="sub-heading" style={{fontSize:60}}>Hi, My name is Manav Jain</h2>
           <p class="paragraph">Enthusiastic React.js developer with a solid foundation in JavaScript, ES6+, and modern frontend
             development practices. Proficient in building responsive, user-friendly web applications using
             React, Redux, and Tailwind CSS. Skilled in integrating APIs and managing state effectively.
